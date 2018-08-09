779855ef29ca5ad1af4fa3d646aadf537c8de226
aretheregods <aretheregods@gmail.com>
2018-08-09 10:51:34 -0400

    Added new & improved IDB code

--- js/idbhelper.js
+++ js/idbhelper.js
@@ -1,86 +1,77 @@
-import idb from 'idb';
+/**
+ * Helper Functions for IDB stuff
+ */
+import idb from "idb";
+
+let dbObject = _openIDBInstance();
 
 /**
- * Helper Class for IDB stuff
+ * Fetch all restaurants from IDB
+ * @return {object|null|void}
  */
-export class IDBHelper {
-  /**
- 	 * Name of the database to use
-   * @return {string}
- 	 */
-  static get DB_NAME() {
-    return 'restaurants-db';
-  }
-
-  /**
-   * Current version number of DB
-   * @return {number}
-   */
-
-  static get DB_VERSION() {
-    return 1;
-  }
-
-  /**
-   * Name of restaurant store in database
-   * @return {string}
-   */ 
-  static get STORE_NAME() {
-    return 'restaurants';
-  }
-
-  /**
-   * DB object on which requests are handled
-   * @return {object}
-   */
-  static get _DBObject() {
-    return IDBHelper.openIDBInstance();
-  }
-
-  /**
-   * Opened database
-   * @return {object}
-   */
-  static openIDBInstance() {
-    // Check that indexeddb is available
-    if (!navigator.serviceWorker) {
-      return Promise.resolve();
-    }
-
-    return idb.open(IDBHelper.DB_NAME, IDBHelper.DB_VERSION, handleDB => {
-      if(!handleDB.objectStoreNames.contains(IDBHelper.STORE_NAME)) {
-        let store = handleDB.createStore(IDBHelper.STORE_NAME, {keyPath: 'id'}).
-        store.createIndex('cuisines', 'cuisine_type');
-        store.createIndex('neighborhoods', 'neighborhood');
-      }
-    })  
-  }
-
-  /**
-   * Adding records to the DB store
-   * @param {Array} restaurants
-   * @return {void}
-   */
-  static putRestaurantsInIDB(restaurants) {
-    IDBHelper._DBObject.then(db => {
-      if(!db) return;
-      const trans = db.transaction('restaurants', 'readwrite');
-      const store = trans.objectStore('restaurants');
-      restaurants.forEach(restaurant => {
-        store.put(restaurant);
-      })
-    })
-  }
-
-  /**
-   * Getting all records from DB store
-   * @return {object} | void
-   */
-  static fetchRestaurantsFromIDB() {
-    return IDBHelper._DBObject.then(db => {
-      if(!db) return;
-      return db.transaction('restaurants').objectStore('restaurants').getAll();
-    })
-  }
-
+export function fetchRestaurantsFromIDB(callback) {
+  return dbObject.then(db => {
+    if (!db) Promise.resolve();
+    const trans = db.transaction("restaurants");
+    const store = trans.objectStore("restaurants")
+    const restaurants = store.getAll();
+    return restaurants;
+  }).catch(e => console.log("Some error:", e));
 }
+
+export function fetchRestaurantFromIDB(id) {
+  return dbObject.then(db => {
+    if (!db) Promise.resolve();
+    const trans = db.transaction("restaurants");
+    const store = trans.objectStore("restaurants");
+    const restaurant = store.get(~~id);
+    return restaurant;
+  }).catch(e => console.log("Some error:", e));
+}
+
+/**
+ * Adding records to the DB store
+ * @param {Array} restaurants
+ */
+export function putRestaurantsInIDB(restaurants) {
+  return dbObject.then(db => {
+    if (!db) return;
+    const trans = db.transaction('restaurants', 'readwrite');
+    const store = trans.objectStore('restaurants');
+    return store.count().then(val => {
+      if (!val) {
+        restaurants.forEach(restaurant => {
+          store.put(restaurant);
+        })
+      } else {
+        return;
+      }
+    })
+  }).catch(e => "Some error or another: " + e);
+}
+
+export function restaurantsInIDB() {
+  return dbObject.then(db => {
+    if (!db) Promise.resolve();
+    const counted = db.transaction('restaurants');
+    const countedStore = counted.objectStore('restaurants');
+    const value = countedStore.count()
+    return value;
+  }).catch(e => { return e});
+}
+
+function _openIDBInstance() {
+  // Check that indexeddb is available
+  if (!navigator.serviceWorker) {
+    return Promise.resolve();
+  }
+
+  return idb.open("restaurants", 1, handleDB => {
+    if(!handleDB.objectStoreNames.contains("restaurants")) {
+      let store = handleDB.createObjectStore("restaurants", { keyPath: 'id' });
+      store.createIndex('cuisines', 'cuisine_type');
+      store.createIndex('neighborhoods', 'neighborhood');
+      return store;
+    }
+  })
+};
