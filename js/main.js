4d84a68f8c9dd88192489343afba04f0800344b9
aretheregods <aretheregods@gmail.com>
2018-08-09 13:05:02 -0400

    Added polyfill for requestIdleCallback

--- js/main.js
+++ js/main.js
@@ -307,3 +307,24 @@ const addMarkersToMap = (restaurants = restaurantsStore.currentRestaurants) => {
     return marker;
   });
 }
+
+/**
+ * Polyfill for if user has a crappy browser
+ */
+window.requestIdleCallback = window.requestIdleCallback ||
+  function (cb) {
+    return setTimeout(function () {
+      var start = Date.now();
+      cb({ 
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        }
+      });
+    }, 1);
+  }
+
+window.cancelIdleCallback = window.cancelIdleCallback ||
+  function (id) {
+    clearTimeout(id);
+  } 
